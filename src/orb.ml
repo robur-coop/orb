306b535d7ac2c388d78adca554d1ef698a0b480a
Robur <team@robur.coop>
2021-06-14 09:37:07 +0000

    adjust allowed paths to include /sbin

--- src/orb.ml
+++ src/orb.ml
@@ -550,7 +550,7 @@ let strip_path () =
   let current_path = try Some (Unix.getenv "PATH") with Not_found -> None in
   let stripped_path =
     let allowed =
-      S.of_list [ "/bin" ; "/usr/bin" ; "/usr/sbin" ; "/usr/local/bin" ; "/opt/bin" ]
+      S.of_list [ "/bin" ; "/sbin" ; "/usr/bin" ; "/usr/sbin" ; "/usr/local/bin" ; "/usr/local/sbin" ]
     in
     let whitelisted x = S.mem x allowed in
     let our_path = match current_path with
